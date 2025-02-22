67c5a249f683938afbcaecdf1c6edc11a534153c
Nastassia27 <anastasia.derugo@gmail.com>
2025-02-22 17:38:59 +0300

    replacing values in string

--- src/8kue/8.js
+++ src/8kue/8.js
@@ -2,9 +2,35 @@
 /*Oh no! Timmy hasn't followed instructions very carefully and ' +
 'forgot how to use the new String Template feature, Help Timmy ' +
 'with his string template so it works as he expects!*/
-
 function buildString(...template){
     return `I like ${template.join(', ')}!`;
 }
 buildString('frog')
 
+/*
+Replace all vowel(гласные) to exclamation mark (восклицательные знаки) in the sentence.
+aeiouAEIOU is vowel.
+"Hi!" --> "H!!"
+"!Hi! Hi!" --> "!H!! H!!"
+"aeiou" --> "!!!!!"
+"ABCDE" --> "!BCD!"*/
+
+function replace(s) {
+    const newS=s.split('')
+    for (let i =0; i<newS.length; i++){
+        if('aeiouAEIOU'.includes(newS[i])){
+            newS[i]='!'
+        }
+    }
+    return newS.join('')
+
+}
+
+//второй вариант
+function replacee(s) {
+    return s.replace(/[aeiou]/gi, '!'); // ✅ Replaces vowels with '!'
+}
+/*/[aeiou]/gi: Matches vowels (a, e, i, o, u)
+g → Global (replaces all occurrences)
+i → Case-insensitive (handles uppercase letters too)*/
+replace("Hi!", "H!!")
