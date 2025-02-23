421f238b12a7b74e076bff58108d2dda91116b29
Nastassia27 <anastasia.derugo@gmail.com>
2025-02-24 00:22:58 +0300

    numbers to string

--- src/8kue/8.js
+++ src/8kue/8.js
@@ -34,3 +34,31 @@ function replacee(s) {
 g → Global (replaces all occurrences)
 i → Case-insensitive (handles uppercase letters too)*/
 replace("Hi!", "H!!")
+
+/*
+
+This function takes two numbers as parameters, the first number
+ being the coefficient, and the second number being the exponent.
+    Your function should multiply the two numbers, and then subtract
+     1 from the exponent. Then, it has to return an expression (like 28x^7).
+     "^1" should not be truncated when exponent = 2.
+
+For example:
+
+    derive(7, 8)
+In this case, the function should multiply 7 and 8, and then subtract 1 from 8.
+It should output "56x^7", the first number 56 being the product of the two numbers,
+and the second number being the exponent minus 1.
+
+*/
+function derive(coefficient,exponent) {
+    const newValue = coefficient*exponent
+    const minus = exponent-1
+    return newValue.toString()+'x^'+minus.toString()
+}
+
+function derive2(coefficient,exponent) {
+    return `${coefficient*exponent}x^${exponent-1}`
+}
+
+derive(7,8)
