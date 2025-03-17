5b8154a5076fc7051de91dc96fbffaf37bad2e43
Nastassia27 <anastasia.derugo@gmail.com>
2025-03-18 00:43:57 +0300

    switch cases

--- src/8kue/8.js
+++ src/8kue/8.js
@@ -164,4 +164,35 @@ function HQ9(code) {
             return undefined
     }
 }
+//simple calculator
 
+function calculator(a,b,sign){
+    if (typeof a ==='number' && typeof b==='number'){
+        switch (sign){
+            case '+':
+                return a+b;
+            case '-':
+                return a-b;
+            case '*':
+                return a*b;
+            case '/':
+                return a/b;
+            default:
+                return "unknown value"
+        } }else {
+        return "unknown value"
+    }
+
+
+}
+/*Create a function that finds the integral of the expression passed.
+
+    In order to find the integral all you need to do is add one to the exponent (the second argument), and divide the coefficient (the first argument) by that new number.
+
+For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, and divided the coefficient by that new number).*/
+function integrate(coefficient, exponent) {
+    let result = ''
+    let newExponent= exponent+1
+    let newCoefficient = coefficient/newExponent
+    return result = `${newCoefficient}x^${newExponent}`
+}
